6fd235aaed36cc1fab80aa6dbbf4f31c95d3ab2f
Friedemann von Lampe <fvonlampe@uni-goettingen.de>
2022-05-10 13:34:47 +0200

    Lesson 3

--- Vegetation_Ecology_and_Multivariate_Analysis/Working_with_tables/initLesson.R
+++ Vegetation_Ecology_and_Multivariate_Analysis/Working_with_tables/initLesson.R
@@ -3,8 +3,9 @@
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
 
+rm(list=ls())
+
 lesson_dir <- file.path(path.package("swirl"), "Courses","Vegetation_Ecology_and_Multivariate_Analysis", "Working_with_tables")
 agroforest_path <- file.path(lesson_dir, "agroforest_var.csv")
-
 
 swirl_options(swirl_is_fun = F)
